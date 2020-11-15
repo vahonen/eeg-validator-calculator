2c5427afb42e641ce15176d2aa022a190cd632f3
Ville Ahonen <ville.ahonen@tuni.fi>
2020-11-15 21:27:46 +0200

    plotted outlier thresholds calculated directly with MAD method

--- Analysis/ratio_boxplot_separate_n_all_recs.m
+++ Analysis/ratio_boxplot_separate_n_all_recs.m
@@ -108,9 +108,13 @@ for kk = 1:numel(chList)
    hold on
    colors = ['r' 'k' 'b' 'g'];
    
+   
    for n = 2:5
-    th = max(rmoutliers(vertcat(origCli{:, n})));
-    yline(th, ['--' colors(n-1)]);
+    yy = vertcat(origCli{:, n}); % combined "n"s
+    maxValueIn(kk,n-1) = max(rmoutliers(yy));
+    madThreshold(kk,n-1) = median(yy)+3*(-1/(sqrt(2)*erfcinv(3/2))) * median(abs(yy-median(yy)));
+    yline(madThreshold(kk,n-1), ['--' colors(n-1)]);
+    
     hold on
    end
    hold off 
